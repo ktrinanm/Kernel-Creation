4412aad300a9017055e1f9f96f8649077d7730f8
ktrinanm <ktrinanl@gmail.com>
2018-01-05 20:50:26 -0700

    Adding multiple tasks

--- kernel.c
+++ kernel.c
@@ -11,25 +11,45 @@ void bwputs(char *s)
 	}
 }
 
+void task()
+{
+	bwputs("In another task\n");
+	
+	while(1) 
+	{
+		syscall();
+	}
+}
+
 void first(void)
 {
 	bwputs("In user mode\n");
 	syscall();
 	bwputs("In user mode time 2\n");
-	syscall();
+	
+	while(1)
+	{
+		syscall();
+	}
 }
 
 int main()
 {
-	unsigned int first_stack[256];
-	unsigned int *first_stack_start = first_stack + 256 - 16;
-	first_stack_start[0] = 0x10;
-	first_stack_start[1] = (unsigned int)&first;
+	unsigned int stacks[2][256];
+	unsigned int *tasks[2];
+
+	tasks[0] = stacks[0] + 256 - 16;
+	tasks[0][0] = 0x10;
+	tasks[0][1] = (unsigned int)&first;
+
+	tasks[1] = stacks[1] + 256 - 16;
+	tasks[1][0] = 0x10;
+	tasks[1][1] = (unsigned int)&task;
 
 	bwputs("Starting....\n");
-	first_stack_start = activate(first_stack_start);
+	activate(tasks[0]);
 	bwputs("Heading back to user mode\n");
-	first_stack_start = activate(first_stack_start);
+	activate(tasks[1]);
 	bwputs("Done.\n");
 
 	while(1); /*because we can't exit the program*/
